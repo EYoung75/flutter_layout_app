63ba0f4a3bd354e15e11626579d9931183002330
EYoung75 <eyoung75.ey@gmail.com>
2019-10-15 14:58:51 -0400

    wrote functionality to filter and display appropriate meals based on category

--- lib/screens/categoryMealsScreen.dart
+++ lib/screens/categoryMealsScreen.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
 
+import "../models/dummy-data.dart";
+
 class CategoryMealsScreen extends StatelessWidget {
   // final String categoryId;
   // final String categoryTitle;
@@ -12,12 +14,20 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs["title"];
     final categoryId = routeArgs["id"];
+    final filteredCategory = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: ListView.builder(),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(filteredCategory[index].title);
+        },
+        itemCount: filteredCategory.length,
+      ),
     );
   }
 }
