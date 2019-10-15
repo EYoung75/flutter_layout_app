597ccc088cf886f47abec14f85899faba41abf92
EYoung75 <eyoung75.ey@gmail.com>
2019-10-15 13:46:56 -0400

    set up basic navigation between all categories screen and CategoryMealsScreen widget

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
               body2: TextStyle(
                 color: Color.fromRGBO(20, 51, 51, 1),
               ),
-              title: TextStyle(fontSize: 24, fontFamily: "RobotoCondensed")
+              title: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, fontFamily: "RobotoCondensed")
             ),
       ),
       home: CategoriesScreen(),
