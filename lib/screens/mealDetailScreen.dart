beb6b1dd91e7cdc131c561678d7b06fde4727069
EYoung75 <eyoung75.ey@gmail.com>
2019-10-15 22:35:18 -0400

    Finished filtering functionality for selecting a single meal from all meal dummy data and beginning to style meal detail/instruction recipe card

--- lib/screens/mealDetailScreen.dart
+++ lib/screens/mealDetailScreen.dart
@@ -1,15 +1,29 @@
 import "package:flutter/material.dart";
 
+import "../models/dummy-data.dart";
+
 class MealDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final mealId = ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final mealId = ModalRoute.of(context).settings.arguments as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Meal Title"),
+        title: Text("${selectedMeal.title}"),
       ),
-      body: Center(
-        child: Text(mealId["id"]),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              selectedMeal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          
+        ],
       ),
     );
   }
--- lib/widgets/mealItem.dart
+++ lib/widgets/mealItem.dart
@@ -52,11 +52,7 @@ class MealItem extends StatelessWidget {
   }
 
   void selectMeal(BuildContext context) {
-    Navigator.of(context).pushNamed("/meal-details", arguments: {"id": id});
-    // Navigator.of(ctx).pushNamed(
-    //   "/meal-category",
-    //   arguments: {"id": id, "title": title},
-    // );
+    Navigator.of(context).pushNamed("/meal-details", arguments: id);
   }
 
   @override
